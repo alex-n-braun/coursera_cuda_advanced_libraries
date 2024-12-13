33db5ff3b08b07ee417cd388d09683a034ca13fd
alex-n-braun <alexnicbraun@gmail.com>
2024-12-13 18:52:28 +0100

    add file for edge detection. implementation in follow-up commit

---
+++ src/edgeDetection.cpp
@@ -0,0 +1,229 @@
+/* Copyright (c) 2019, NVIDIA CORPORATION. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *  * Neither the name of NVIDIA CORPORATION nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#if defined(WIN32) || defined(_WIN32) || defined(WIN64) || defined(_WIN64)
+#define WINDOWS_LEAN_AND_MEAN
+#define NOMINMAX
+#include <windows.h>
+#pragma warning(disable : 4819)
+#endif
+
+#include <Exceptions.h>
+#include <ImageIO.h>
+#include <ImagesCPU.h>
+#include <ImagesNPP.h>
+
+#include <string.h>
+#include <fstream>
+#include <iostream>
+
+#include <cuda_runtime.h>
+#include <npp.h>
+
+#include <helper_cuda.h>
+#include <helper_string.h>
+
+bool printfNPPinfo()
+{
+    const NppLibraryVersion *libVer = nppGetLibVersion();
+
+    printf("NPP Library Version %d.%d.%d\n", libVer->major, libVer->minor,
+           libVer->build);
+
+    int driverVersion, runtimeVersion;
+    cudaDriverGetVersion(&driverVersion);
+    cudaRuntimeGetVersion(&runtimeVersion);
+
+    printf("  CUDA Driver  Version: %d.%d\n", driverVersion / 1000,
+           (driverVersion % 100) / 10);
+    printf("  CUDA Runtime Version: %d.%d\n", runtimeVersion / 1000,
+           (runtimeVersion % 100) / 10);
+
+    // Min spec is SM 1.0 devices
+    bool bVal = checkCudaCapabilities(1, 0);
+    return bVal;
+}
+
+struct Cli {
+    Cli(int argc, char* argv[]) {
+        char *filePath;
+        if (checkCmdLineFlag(argc, (const char **)argv, "input"))
+        {
+            getCmdLineArgumentString(argc, (const char **)argv, "input", &filePath);
+        }
+        else
+        {
+            filePath = sdkFindFilePath("Lena.pgm", argv[0]);
+        }
+        if (filePath)
+        {
+            filename = filePath;
+        }
+        else
+        {
+            filename = "Lena.pgm";
+        }
+
+        // if we specify the filename at the command line, then we only test
+        // filename[0].
+        int file_errors = 0;
+        std::ifstream infile(filename, std::ifstream::in);
+
+        if (infile.good())
+        {
+            std::cout << "nppiRotate opened: <" << filename
+                      << "> successfully!" << std::endl;
+            file_errors = 0;
+            infile.close();
+        }
+        else
+        {
+            std::cout << "nppiRotate unable to open: <" << filename << ">"
+                      << std::endl;
+            file_errors++;
+            infile.close();
+        }
+
+        if (file_errors > 0)
+        {
+            exit(EXIT_FAILURE);
+        }
+
+        resultFilename = filename;
+        std::string::size_type dot = resultFilename.rfind('.');
+        if (dot != std::string::npos)
+        {
+            resultFilename = resultFilename.substr(0, dot);
+        }
+        resultFilename += "_rotate.pgm";
+        if (checkCmdLineFlag(argc, (const char **)argv, "output"))
+        {
+            char *outputFilePath;
+            getCmdLineArgumentString(argc, (const char **)argv, "output",
+                                     &outputFilePath);
+            resultFilename = outputFilePath;
+        }
+
+        angle = -24.0; // Rotation angle in degrees
+        if (checkCmdLineFlag(argc, (const char **)argv, "angle"))
+        {
+            char *outputFilePath;
+            angle = getCmdLineArgumentFloat(argc, (const char **)argv, "angle");
+        }
+
+    }
+
+    std::string filename;
+    std::string resultFilename;
+    double angle;
+};
+
+int main(int argc, char *argv[])
+{
+    printf("%s Starting...\n\n", argv[0]);
+
+    try
+    {
+        findCudaDevice(argc, (const char **)argv);
+
+        if (printfNPPinfo() == false)
+        {
+            exit(EXIT_SUCCESS);
+        }
+
+        Cli cli{argc, argv};
+        std::string filename = cli.filename;
+        double angle = cli.angle;
+        std::string resultFilename = cli.resultFilename;
+
+        // declare a host image object for an 8-bit grayscale image
+        npp::ImageCPU_8u_C1 oHostSrc;
+        // load gray-scale image from disk
+        npp::loadImage(filename, oHostSrc);
+        // declare a device image and copy construct from the host image,
+        // i.e. upload host to device
+        npp::ImageNPP_8u_C1 oDeviceSrc(oHostSrc);
+
+        // create struct with the ROI size
+        NppiRect oSrcRect = {0, 0, (int)oDeviceSrc.width(), (int)oDeviceSrc.height()};
+        NppiSize oSrcSize = {oSrcRect.width, oSrcRect.height};
+        NppiRect oSrcOffset = {0, 0, oSrcRect.width, oSrcRect.height};
+        NppiSize oSizeROI = {(int)oDeviceSrc.width(), (int)oDeviceSrc.height()};
+
+        // Calculate the bounding box of the rotated image
+        double aBoundingBox[2][2];
+        NppiRect oBoundingBox;
+        NPP_CHECK_NPP(nppiGetRotateBound(oSrcRect, aBoundingBox, angle, 0, 0));
+        oBoundingBox.x = 0;
+        oBoundingBox.y = 0;
+        oBoundingBox.width = aBoundingBox[1][0] - aBoundingBox[0][0];
+        oBoundingBox.height = aBoundingBox[1][1] - aBoundingBox[0][1];
+
+        // allocate device image for the rotated image
+        npp::ImageNPP_8u_C1 oDeviceDst(oBoundingBox.width, oBoundingBox.height);
+
+        auto inputStep = oDeviceSrc.pitch();
+        auto outputStep = oDeviceDst.pitch();
+        auto xShift = -aBoundingBox[0][0];
+        auto yShift = -aBoundingBox[0][1];
+        // run the rotation
+        NPP_CHECK_NPP(nppiRotate_8u_C1R(
+            oDeviceSrc.data(), oSrcSize, inputStep, oSrcOffset,
+            oDeviceDst.data(), outputStep, oBoundingBox, angle, xShift, yShift,
+            NPPI_INTER_NN));
+
+        // declare a host image for the result
+        npp::ImageCPU_8u_C1 oHostDst(oDeviceDst.size());
+        // and copy the device result data into it
+        oDeviceDst.copyTo(oHostDst.data(), oHostDst.pitch());
+
+        saveImage(resultFilename, oHostDst);
+        std::cout << "Saved image: " << resultFilename << std::endl;
+
+        nppiFree(oDeviceSrc.data());
+        nppiFree(oDeviceDst.data());
+
+        exit(EXIT_SUCCESS);
+    }
+    catch (npp::Exception &rException)
+    {
+        std::cerr << "Program error! The following exception occurred: \n";
+        std::cerr << rException << std::endl;
+        std::cerr << "Aborting." << std::endl;
+
+        exit(EXIT_FAILURE);
+    }
+    catch (...)
+    {
+        std::cerr << "Program error! An unknown type of exception occurred. \n";
+        std::cerr << "Aborting." << std::endl;
+
+        exit(EXIT_FAILURE);
+    }
+
+    return 0;
+}
